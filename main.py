863d848ea4131786d6be2ba129db5bb463a980fa
Зайцев Максим <max11zaycev@gmail.com>
2022-10-21 14:19:27 +0700

    Yp leson 2-7-2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+"""Игра - симулятор гладиаторской арены."""
+
 from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Модуль атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Модуль защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Модуль специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +38,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Модуль начала или окончания тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +63,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Модуль выбора класса персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -77,8 +84,6 @@ def choice_char_class() -> str:
                                'чтобы выбрать другого персонажа ').lower()
     return char_class
 
-
-def main():
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,6 +93,4 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
+    
--- temp.py
+++ temp.py
@@ -1,40 +1,22 @@
-# Тестовые данные.
-TEST_DATA: list[tuple[int, str, bool]] = [
-    (44, 'success', True),
-    (16, 'failure', True),
-    (4, 'success', False),
-    (21, 'failure', False),
-]
-
-BONUS: float = 1.1
-ANTIBONUS: float = 0.8
+import math, itertools
+# import itertools
 
 
-def add_rep(current_rep: float, rep_points: int, buf_effect: bool) -> float:
-    current_rep += rep_points
-    if buf_effect:
-        return current_rep * BONUS
-    return current_rep
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+          'квадратного корня из заданного числа')
+print (message)
+
+def  CalculateSquareRoot (Number ):
+    """ Вычисляет квадратный корень"""
+    return  sqrt(Number )
+
+def calc(your_number) :
+    if your_number<=0:
+        return    
+     
+    root = 0
+    print(f"Мы вычислили квадратный корень из введённого вами числа. Это будет: {CalculateSquareRoot(your_number)}")
 
 
-def remove_rep(current_rep: float, rep_points: int,
-               debuf_effect: bool) -> float:
-    current_rep -= rep_points
-    if debuf_effect:
-        return current_rep * ANTIBONUS
-    return current_rep
-
-
-def main(duel_res: list) -> str:
-    current_rep: float = 0.0
-    for rep, result, effect in duel_res:
-        if result == 'success':
-            current_rep = add_rep(current_rep, rep, effect)
-        if result == 'failure':
-            current_rep = remove_rep(current_rep, rep, effect)
-    return (f'После {len(duel_res)} поединков,'
-            f'репутация персонажа — {current_rep:.3f} очков.')
-
-
-# Тестовый вызов функции main.
-print(main(TEST_DATA))
+print(message)
+calc (25.5)
